d6f491627e7ecc95911f2f45ab3b8a52d6a5dd8d
frrst-ian <frrstxd@proton.me>
2025-11-22 21:49:13 +0800

    chore: add start script

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(
                 "http://localhost:3001",
                 "http://localhost:5173",
                 "http://localhost:5174",
+                "https://messz.onrender.com"
             ];
 
             if (allowedOrigins.includes(origin)) {
