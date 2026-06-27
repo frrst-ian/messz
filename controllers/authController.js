bd1be29eec60aa0a3332f82ff55ebe45402bf520
frrst-ian <frrstxd@proton.me>
2026-06-27 18:58:59 +0800

    fix: use correct var name for import

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const passport = require("../config/passport");
-const passport = require("../config/cloudinary");
+const cloudinary = require("../config/cloudinary");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const db = require("../db/queries");
